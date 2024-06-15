ed6bbaead608a41f4b7430ee5033d668bf99d32c
github-practice-pw <githubpractice4interview@gmail.com>
2024-06-14 23:06:41 -0700

    Completed iterative quick sort

--- Exercise_4.cpp
+++ Exercise_4.cpp
@@ -1,3 +1,13 @@
+/*
+ *                     Merge Sort Alorithm
+ *
+ * Time Complexity  :  O(nlogn)
+ * Space Complexity :  O(n) 
+ * Did this code successfully run on Leetcode/VSCode : Yes
+ * Any problem you faced while coding this : No 
+ * Your code here along with comments explaining your approach :
+ *          Using divide and conquer technique.
+ */
 #include<iostream>
   
 // Merges two subarrays of arr[]. 
@@ -6,6 +16,41 @@
 void merge(int arr[], int l, int m, int r) 
 { 
     //Your code here
+    int nL = m - l + 1;
+    int nR = r - m;
+
+    int L[nL], R[nR];
+
+    for(int i = 0; i < nL; i++) {
+        L[i] = arr[l+i];
+    }
+    for(int i = 0; i < nR; i++) {
+        R[i] = arr[m + 1 + i];
+    }
+
+    int i = 0, j = 0;
+    int k = l;
+
+    while (i < nL && j < nR) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < nL) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+    while (j < nR) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
 } 
   
 /* l is for left index and r is right index of the 
@@ -13,6 +58,12 @@ void merge(int arr[], int l, int m, int r)
 void mergeSort(int arr[], int l, int r) 
 { 
     //Your code here
+    if (l < r) {
+        int mid = l + (r-l)/2;
+        mergeSort(arr, l, mid);
+        mergeSort(arr, mid+1, r);
+        merge(arr, l, mid, r);
+    }
 } 
   
 /* UTILITY FUNCTIONS */
--- Exercise_5.cpp
+++ Exercise_5.cpp
@@ -1,4 +1,20 @@
+/*
+ *                     Iterative way of using Quick sort algorithm 
+ *
+ * Leetcode : 
+ *
+ * Time Complexity  : O(nlogn) and O(n^2)-->Worst case 
+ * Space Complexity : O(logn)  and O(n)--> Worse Case
+ * Did this code successfully run on Leetcode/VSCode : Yes
+ * Any problem you faced while coding this : No
+ * Your code here along with comments explaining your approach :
+ *          Using stacks to manage sub-array boundaries
+ *          Using pivot to partition into sub-arrays 
+ *          By pushing and popping indices into stack, iteratively sorting the entire array.
+ */
+
 #include <iostream>
+#include <stack>
 using namespace std; 
   
 // A utility function to swap two elements 
@@ -12,16 +28,57 @@ void swap(int* a, int* b)
 /* This function is same in both iterative and recursive*/
 int partition(int arr[], int l, int h) 
 { 
-    //Do the comparison and swapping here 
+    //Do the comparison and swapping here
+    int pivot = arr[h];
+    int pIndex = l;
+       for(int i = l; i < h; i++) {
+        if (arr[i] <= pivot) {
+            swap(&arr[i], &arr[pIndex]);
+            pIndex++;
+        }
+    }
+    swap(&arr[pIndex], &arr[h]);
+    return pIndex;
 } 
   
 /* A[] --> Array to be sorted,  
 l --> Starting index,  
 h --> Ending index */
-void quickSortIterative(int arr[], int l, int h) 
-{ 
-    //Try to think that how you can use stack here to remove recursion.
-} 
+// Iterative Quick Sort function
+void quickSortIterative(int arr[], int low, int high) {
+    // Create an auxiliary stack
+    stack<int> stk;
+
+    // Push initial values of low and high to stack
+    stk.push(low);
+    stk.push(high);
+
+    // Keep popping from stack while it is not empty
+    while (!stk.empty()) {
+        // Pop high and low
+        high = stk.top();
+        stk.pop();
+        low = stk.top();
+        stk.pop();
+
+        // Set pivot element at its correct position
+        int p = partition(arr, low, high);
+
+        // If there are elements on the left side of the pivot,
+        // push left side to stack
+        if (p - 1 > low) {
+            stk.push(low);
+            stk.push(p - 1);
+        }
+
+        // If there are elements on the right side of the pivot,
+        // push right side to stack
+        if (p + 1 < high) {
+            stk.push(p + 1);
+            stk.push(high);
+        }
+    }
+}
   
 // A utility function to print contents of arr 
 void printArr(int arr[], int n) 
